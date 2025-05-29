cfba58d1428ba644b5a1f4d022e4513d99151e29
whgkswo <gjuk7238@gmail.com>
2025-05-29 20:59:40 +0900

    1. 프로젝트명 변경

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,8 +4,17 @@ export default function Home() {
 
     return (
         <div className={css.main}>
-            <section className={css.container}>
-                <h2>내용을 채워야 함</h2>
+            <section className={css.section}>
+                <header className={css.section_header}>
+                    <h3>최근 인기 카드</h3>
+                    <button className={css.more_button}>+ 더보기</button>
+                </header>
+            </section>
+            <section className={css.section}>
+                <header className={css.section_header}>
+                    <h3>명예의 전당</h3>
+                    <button className={css.more_button}>+ 더보기</button>
+                </header>
             </section>
         </div>
     )
